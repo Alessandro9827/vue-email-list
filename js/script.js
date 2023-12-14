30f82c02d15d79ce24ab8fc89f5fa959640657d7
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-14 09:57:03 +0100

    push exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 //Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emails:[]
+        }
+    },
+
+    methods: {
+        getEmail(){
+            for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
+                (response) =>{
+                console.log(this.emails);
+                this.emails.push(response.data.response)
+            });
+        }
+        }
+    },
+
+
+    created() {
+        this.getEmail();
+    },
+
+}).mount('#app');
